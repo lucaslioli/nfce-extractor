dff4eef9132f91c4b4de5fd11f66bdcd58cb3472
lucaslioli <lucaslima.o@outlook.com>
2017-09-21 14:50:02 -0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,3 @@ Route::get('/extractor', 'NfceController@extractor');
 Route::post('/data_extract','NfceController@data_extract');
 
 Route::get('/nfce/{id}','NfceController@show');
-
-// Route::get('/nfce/{id}', function($id){
-// 	$nfce = DB::table('nfces')->find($id);
-    	
-// 	return view('nfces.show', compact('nfce'));
-// });
-
