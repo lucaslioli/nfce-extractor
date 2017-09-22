4f1343027f9bdfa0c69f822caaac2ad8e441780e
lucaslioli <lucaslima.o@outlook.com>
2017-09-21 21:43:01 -0300

    Added information about where to find the access key

--- resources/views/extractor.blade.php
+++ resources/views/extractor.blade.php
@@ -14,17 +14,18 @@
 		</div>
 		<button type="submit" class="btn btn-outline-primary btn-block">Cadastrar</button>
 	</form>
-	<!-- GIF Loading -->
-	<div class="col-md-offset-3 col-md-6 text-center" id="loading" style="display: none">
-		<img src="https://d2x5ku95bkycr3.cloudfront.net/img/loading.gif">
+	<div>
+		<br/>
+		<h5>Onde encontrar a Chave de Acesso</h5>
+		<p>Número de 44 digitos, encontrado geralmente na parte superior ou inferior da	Nota, próximo aos dados do consumidor e junto dos demai dados referentes a própria NFC-e.</p>
+		<div class="text-center">
+			<img src="example-nfce.jpg" width="500px">
+		</div>
+		<br/>
 	</div>
 	<!-- Mensagem de Alerta -->
 	<div class="form-group" id="div_mensagem" style="display: none">
 		<div id="mensagem" class="text-center">
 		</div>
-	</div>
-	<!-- Resultado -->
-	<div class="col-md-12">
-		<div class="form-group col-md-offset-3 col-md-6" id="div_resultado"></div>
 	</div>
 @endsection
